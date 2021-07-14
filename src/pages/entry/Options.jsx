49a06536e85e8d871431917d57669a579ecb2662
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-07-14 10:47:55 -0300

    feat: show scoop options images from server

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { ScoopOptions } from './ScoopOption'
+import { Row } from 'react-bootstrap'
+
+export function Options({ optionType }) {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => console.log(error))
+  }, [optionType, setItems])
+
+  const ItemComponent = Object.is(optionType, 'scoops') ? ScoopOptions : null
+
+  const optionItems = items.map((eachItem) => (
+    <ItemComponent
+      key={eachItem.name}
+      name={eachItem.name}
+      imagePath={eachItem.imagePath}
+    />
+  ))
+
+  return <Row>{optionItems}</Row>
+}
